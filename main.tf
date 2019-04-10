5b13d4dc6901cef413d9b27d2c82e6668ef4b1f1
David McPike <david.mcpike@clearesult.com>
2019-04-09 19:38:19 -0500

    local environment defaults

--- main.tf
+++ main.tf
@@ -1,41 +1,24 @@
-# provider "azurerm" {
-#   subscription_id = "REPLACE-WITH-YOUR-SUBSCRIPTION-ID"
-#   client_id       = "REPLACE-WITH-YOUR-CLIENT-ID"
-#   client_secret   = "REPLACE-WITH-YOUR-CLIENT-SECRET"
-#   tenant_id       = "REPLACE-WITH-YOUR-TENANT-ID"
-# }
-
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group_name}"
-  location = "${var.location}"
-}
-
-resource "azurerm_sql_database" "db" {
-  name                             = "${var.db_name}"
-  resource_group_name              = "${azurerm_resource_group.rg.name}"
-  location                         = "${var.location}"
-  edition                          = "${var.db_edition}"
-  collation                        = "${var.collation}"
-  server_name                      = "${azurerm_sql_server.server.name}"
-  create_mode                      = "Default"
-  requested_service_objective_name = "${var.service_objective_name}"
-  tags                             = "${var.tags}"
+# can be used for additional accounts
+resource "random_string" "password" {
+  length           = 16
+  special          = true
+  override_special = "/@\" "
 }
 
 resource "azurerm_sql_server" "server" {
-  name                         = "${var.db_name}-sqlsvr"
-  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  name                         = "${local.name}${format("%3d", count.index)}"
+  count                        = "${var.count}"
+  resource_group_name          = "${var.rg_name}"
   location                     = "${var.location}"
   version                      = "${var.server_version}"
   administrator_login          = "${var.sql_admin_username}"
-  administrator_login_password = "${var.sql_password}"
-  tags                         = "${var.tags}"
-}
+  administrator_login_password = "${local.sql_admin_password}"
 
-resource "azurerm_sql_firewall_rule" "fw" {
-  name                = "${var.db_name}-fwrules"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  server_name         = "${azurerm_sql_server.server.name}"
-  start_ip_address    = "${var.start_ip_address}"
-  end_ip_address      = "${var.end_ip_address}"
+  tags = {
+    InfrastructureAsCode = "True"
+  }
+
+  lifecycle {
+    ignore_changes = ["administrator_login_password", "tags"]
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,28 @@
-variable "resource_group_name" {
-  description = "Default resource group name that the database will be created in."
-  default     = "myapp-rg"
+variable "rgid" {
+  description = "RGID used for naming"
 }
 
 variable "location" {
-  description = "The location/region where the database and server are created. Changing this forces a new resource to be created."
+  default     = "southcentralus"
+  description = "Location for resources to be created"
+}
+
+variable "count" {
+  default = "1"
+}
+
+variable "name_prefix" {
+  default     = ""
+  description = "Allows users to override the standard naming prefix.  If left as an empty string, the standard naming conventions will apply."
+}
+
+variable "environment" {
+  default     = "dev"
+  description = "Environment used in naming lookups"
+}
+
+variable "rg_name" {
+  description = "Default resource group name that the database will be created in."
 }
 
 variable "server_version" {
@@ -12,49 +30,37 @@ variable "server_version" {
   default     = "12.0"
 }
 
-variable "db_name" {
-  description = "The name of the database to be created."
-}
-
-variable "db_edition" {
-  description = "The edition of the database to be created."
-  default     = "Basic"
-}
-
-variable "service_objective_name" {
-  description = "The performance level for the database. For the list of acceptable values, see https://docs.microsoft.com/en-gb/azure/sql-database/sql-database-service-tiers. Default is Basic."
-  default     = "Basic"
-}
-
-variable "collation" {
-  description = "The collation for the database. Default is SQL_Latin1_General_CP1_CI_AS"
-  default     = "SQL_Latin1_General_CP1_CI_AS"
-}
-
 variable "sql_admin_username" {
   description = "The administrator username of the SQL Server."
 }
 
-variable "sql_password" {
-  description = "The administrator password of the SQL Server."
+variable "use_random_password" {
+  default     = true
+  description = "Default is true. If true, deploy the Azure SQL server with a randomly created password that will be visible in the state.  If false, you must have a valid vault_name and secret_name variable entered, which will supply the password."
 }
 
-variable "start_ip_address" {
-  description = "Defines the start IP address used in your database firewall rule."
-  default     = "0.0.0.0"
+variable "key_vault_pw" {
+  default     = ""
+  description = "Pass in a custom password, such as an Azure Key Vault data reference."
 }
 
-variable "end_ip_address" {
-  description = "Defines the end IP address used in your database firewall rule."
-  default     = "0.0.0.0"
+# Compute default name values
+locals {
+  env_id = "${lookup(module.naming.env-map, var.environment, "ENV")}"
+  type   = "${lookup(module.naming.type-map, "azurerm_sql_server", "TYP")}"
+
+  default_rgid        = "${var.rgid != "" ? var.rgid : "NORGID"}"
+  default_name_prefix = "c${local.default_rgid}${local.env_id}"
+
+  name_prefix = "${var.name_prefix != "" ? var.name_prefix : local.default_name_prefix}"
+  name        = "${local.name_prefix}${local.type}"
+
+  random_pw          = "${sha256(bcrypt(random_string.password.result))}"
+  key_vault_pw       = "${data.azurerm_key_vault_secret.admin_password.value}"
+  sql_admin_password = "${var.use_random_password == true ? local.random_pw : var.key_vault_pw}"
 }
 
-variable "tags" {
-  description = "The tags to associate with your network and subnets."
-  type        = "map"
-
-  default = {
-    tag1 = ""
-    tag2 = ""
-  }
+# This module provides a data map output to lookup naming standard references
+module "naming" {
+  source = "git::ssh://git@github.com/clearesult/cr-azurerm-naming.git?ref=v1.0"
 }
