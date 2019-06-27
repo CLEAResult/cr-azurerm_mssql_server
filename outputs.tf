f2a3f4d2022d5de3b64086e72036f545a58c4c25
David McPike <david.mcpike@clearesult.com>
2019-06-27 10:31:33 -0500

    Version 1.4.2
    - Add sql_server_id output
    - Remove 0.12 "list of lists" output operators
    - Add tests for all outputs

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,25 @@
+output "sql_server_id" {
+  description = "Azure resource ID of the Azure SQL Database created."
+  value       = azurerm_sql_server.server.*.id
+}
+
 output "sql_server_name" {
   description = "Server name of the Azure SQL Database created."
-  value       = [azurerm_sql_server.server.*.name]
+  value       = azurerm_sql_server.server.*.name
 }
 
 output "sql_server_location" {
   description = "Location of the Azure SQL Database created."
-  value       = [azurerm_sql_server.server.*.location]
+  value       = azurerm_sql_server.server.*.location
 }
 
 output "sql_server_version" {
   description = "Version the Azure SQL Database created."
-  value       = [azurerm_sql_server.server.*.version]
+  value       = azurerm_sql_server.server.*.version
 }
 
 output "sql_server_fqdn" {
   description = "Fully Qualified Domain Name (FQDN) of the Azure SQL Database created."
-  value       = [azurerm_sql_server.server.*.fully_qualified_domain_name]
+  value       = azurerm_sql_server.server.*.fully_qualified_domain_name
 }
 
--- test/fixture/outputs.tf
+++ test/fixture/outputs.tf
@@ -1,8 +1,25 @@
-output "sql_admin_username" {
-  value = var.sql_admin_username
+output "id" {
+  description = "Azure resource ID of the Azure SQL Database created."
+  value       = module.sql-server.*.sql_server_id
 }
 
-output "sql_password" {
-  value = var.sql_admin_password
+output "name" {
+  description = "Server name of the Azure SQL Database created."
+  value       = module.sql-server.*.sql_server_name
+}
+
+output "location" {
+  description = "Location of the Azure SQL Database created."
+  value       = module.sql-server.*.sql_server_location
+}
+
+output "version" {
+  description = "Version the Azure SQL Database created."
+  value       = module.sql-server.*.sql_server_version
+}
+
+output "fqdn" {
+  description = "Fully Qualified Domain Name (FQDN) of the Azure SQL Database created."
+  value       = module.sql-server.*.sql_server_fqdn
 }
 
